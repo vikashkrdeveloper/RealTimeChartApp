54299dd86f8a949cf3e15edcac31d298e9561326
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2023-11-05 23:10:05 +0530

    RealTimeApp

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = express();
 const router = require('../routes/Router');
 const port = process.env.PORT || 5000;
 const cookieParser = require('cookie-parser');
+const path=require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
