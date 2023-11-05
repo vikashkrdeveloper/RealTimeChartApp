c2fc3191f2e0350e71eef917fe485b67a5fa8781
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2023-11-05 23:15:05 +0530

    HomeControllers.js

--- controllers/HomeControllers.js
+++ controllers/HomeControllers.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HomeControllers = (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+    res.status(200).send('Home Page');
 }
 
 module.exports = HomeControllers;
